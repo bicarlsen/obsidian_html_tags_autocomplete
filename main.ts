0079a28ef84c8a5024631f87ff8c542fe81b9f41
Brian Carlsen <carlsen.bri@gmail.com>
2021-10-20 12:06:45 +0200

    Initial commit. Basic functionality working.

--- main.ts
+++ main.ts
@@ -1,79 +1,32 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-interface MyPluginSettings {
-	mySetting: string;
+interface HtmlTagsAutocompleteSettings {
+	debug_mode: boolean;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: HtmlTagsAutocompleteSettings = {
+	debug_mode: false
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class HtmlTagsAutocomplete extends Plugin {
+	private listening: boolean;
+	private tag: string = '';
+	private tag_pattern: RegExp;
+
+	settings: HtmlTagsAutocompleteSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		let ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		let statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				let markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new HtmlTagsAutocompleteSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+		this.registerCodeMirror( ( editor ) => {
+			editor.on( 'keypress', this.keyPressListener );
+		} );
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.tag_pattern = /[\w\d]/i
+
 	}
 
 	onunload() {
@@ -87,28 +40,77 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	private keyPressListener = (
+		editor: CodeMirror.Editor,
+		event: KeyboardEvent
+	) => {
+		const key = event.key;
+
+		if ( key === '<' ) {
+			// opening tag
+			if ( this.listening ) {
+				// invalid tag, ignore
+				if ( this.settings.debug_mode ) {
+					console.log( 'Invalid tag key.' );
+				}
+
+				this.reset();
+				return;
+			}
+
+			this.listening = true;
+		}
+
+		else if ( this.listening && ( key === '>' ) ) {
+			// ending tag
+			if ( this.settings.debug_mode ) {
+				console.log( `Inserting tag: ${this.tag}.` );
+			}
+
+			const pos = editor.getCursor();
+			editor.replaceRange( `</${this.tag}>`, pos );
+			
+			// reset cursor to original position inside tag
+			editor.setCursor( pos ); 
+			
+			this.reset();
+			return;
+		}
+
+		else if ( this.listening ) {
+			if ( this.tag_pattern.test( key ) ) {
+				// valid tag key
+				if ( this.settings.debug_mode ) {
+					console.log( `Key match: ${key}.` );
+				}
+
+				this.tag += key;
+			}
+			else {
+				// invalid tag key
+				if ( this.settings.debug_mode ) {
+					console.log( 'Invalid tag key.' );
+				}
+
+				this.reset()
+				return;
+			}
+		}
+
+	}
+
+
+	private reset = () => {
+		this.listening = false;
+		this.tag = '';
+	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class HtmlTagsAutocompleteSettingTab extends PluginSettingTab {
+	plugin: HtmlTagsAutocomplete;
 
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		let {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: HtmlTagsAutocomplete) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -118,17 +120,15 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'Developer settings'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+			.setName('Debug mode')
+			.setDesc('Log debug messages to the console.')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.debug_mode)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.debug_mode = value;
 					await this.plugin.saveSettings();
 				}));
 	}
